155d36793c02989ead7283b9f7eb93b7727ac3a2
👾GhurbeSABZI <34764281+qewr1324@users.noreply.github.com>
2024-11-07 15:56:24 +0330

    📌 for reasons
    🪙 have fun..!

---
+++ editor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(minifier))]
+public class editor : Editor {
+
+	// file name
+	string name;
+
+	// for inspector
+	bool file;
+
+	// GUI
+	public override void OnInspectorGUI(){
+		minifier _test = (minifier)target;
+		EditorGUILayout.Space(); // sapce
+
+		// label
+		GUILayout.Box ("C# Minifier", GUILayout.ExpandWidth(true));
+		EditorGUILayout.Space(); // sapce
+
+		if(_test.check) {
+			// ***  author of this project  ***
+			EditorGUILayout.TextArea ("Success..!!\n\nwww.github.com/qewr1324\nwww.artstation.com/gurbesabzi\nauthor : Amirhussein Mohammadi Fard\n\nApache License\nVersion 2.0, January 2004\nhttp://www.apache.org/licenses/\n\n\nVersion 1.4.0");
+			EditorGUILayout.Space(); // sapce
+		}
+		else{
+			// note message
+			EditorGUILayout.HelpBox ("\nﺪﯿﻨﮐ ﻑﺬﺣ #C ﺖﭙﯾﺮﮑﺳﺍ ﺭﺩ ﺍﺭ ﺎﻫ Comment ﻡﺎﻤﺗ ،ﻞﯾﺎﻓ ﻥﺩﺮﮐ ﺩﺭﺍﻭ ﺯﺍ ﻞﺒﻗ ًﺎﻔﻄﻟ\n\nPlease Delete All COMMENTs Text In The C# Script, Before You Import File\n", MessageType.Warning);
+		}
+
+		if (!_test.group) {
+			GUILayout.BeginHorizontal (); // fix begin scale
+			// import C# file
+			if(GUILayout.Button("Import File\n*cs script*")){
+				if (file) {
+					if (name != null) _test.reader (); else EditorUtility.DisplayDialog ("Note", "یک نام برای فایل خود انتخاب کنید\n\nChoose a name for your file", "ok");
+				} else _test.reader ();
+			}
+			// do minify process
+			if(GUILayout.Button("Process\nFile")){
+				_test.proccess ();
+			}
+			GUILayout.EndHorizontal (); // fix end scale
+			EditorGUILayout.Space(); // sapce
+		}
+		else{
+			// clear all variables and paths
+			if(GUILayout.Button("Clear\n*Minifier*")){
+				_test.refreshing ();
+			}
+		}
+	}
+}
---
+++ minifier.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class minifier : MonoBehaviour 
+{
+	// C# file.
+	TextAsset file;
+
+	// text input processing.
+	string process;
+	string stream;
+
+	// for checking whitespace in script.
+	byte space;
+
+	// file address.
+	internal string path;
+	internal bool check , group;
+
+	// read the cs file.
+	internal void reader ()
+	{
+		try{
+			// refreshing data.
+			check = false;
+			group = false;
+			refresh ();
+
+			// opens File Explorer and gets the file path.
+			path = EditorUtility.OpenFilePanel ("Choose Your *Cs File(.cs)", "", "cs");
+		}
+		catch{
+			// to show the dialog page in unity.
+			if (path == "") EditorUtility.DisplayDialog ("Note", "You must select a C# file", "ok");
+		}
+	}
+	// process the cs file.
+	internal void proccess()
+	{
+		try{
+			// get object file as a TestAsset type.
+			file = (TextAsset)AssetDatabase.LoadAssetAtPath("File://" + path.Replace("\\", "/").Trim(), typeof(TextAsset)) as TextAsset;
+
+			// the entire script reads the line and sets it to string.
+			string stream = File.ReadAllText (path);
+
+			// remove extra lines in script.
+			stream.Replace("\n", "");
+
+			// clears whitespace and preserves the script.
+			for (int b = 0; b < stream.Length; b++) {
+				switch (stream [b])
+				{
+				case ' ': if(stream [b + 1] != ' ') process += stream [b]; break;
+				case '.': process += stream [b]; break;
+				case '=': process += stream [b]; break;
+				case '+': process += stream [b]; break;
+				case ':': process += stream [b]; break;
+				case '[': process += stream [b]; break;
+				case ']': process += stream [b]; break;
+				case '(': process += stream [b]; break;
+				case ')': process += stream [b]; break;
+				case '{': process += stream [b]; break;
+				case '}': process += stream [b]; break;
+				case '<': process += stream [b]; break;
+				case '>': process += stream [b]; break;
+				case ';': process += stream [b]; break;
+				case 'a': process += stream [b]; break;
+				case 'b': process += stream [b]; break;
+				case 'c': process += stream [b]; break;
+				case 'd': process += stream [b]; break;
+				case 'e': process += stream [b]; break;
+				case 'f': process += stream [b]; break;
+				case 'g': process += stream [b]; break;
+				case 'h': process += stream [b]; break;
+				case 'i': process += stream [b]; break;
+				case 'j': process += stream [b]; break;
+				case 'k': process += stream [b]; break;
+				case 'l': process += stream [b]; break;
+				case 'm': process += stream [b]; break;
+				case 'n': process += stream [b]; break;
+				case 'o': process += stream [b]; break;
+				case 'p': process += stream [b]; break;
+				case 'q': process += stream [b]; break;
+				case 'r': process += stream [b]; break;
+				case 's': process += stream [b]; break;
+				case 't': process += stream [b]; break;
+				case 'u': process += stream [b]; break;
+				case 'v': process += stream [b]; break;
+				case 'w': process += stream [b]; break;
+				case 'x': process += stream [b]; break;
+				case 'y': process += stream [b]; break;
+				case 'z': process += stream [b]; break;
+				case 'A': process += stream [b]; break;
+				case 'B': process += stream [b]; break;
+				case 'C': process += stream [b]; break;
+				case 'D': process += stream [b]; break;
+				case 'E': process += stream [b]; break;
+				case 'F': process += stream [b]; break;
+				case 'G': process += stream [b]; break;
+				case 'H': process += stream [b]; break;
+				case 'I': process += stream [b]; break;
+				case 'J': process += stream [b]; break;
+				case 'K': process += stream [b]; break;
+				case 'L': process += stream [b]; break;
+				case 'M': process += stream [b]; break;
+				case 'N': process += stream [b]; break;
+				case 'O': process += stream [b]; break;
+				case 'P': process += stream [b]; break;
+				case 'Q': process += stream [b]; break;
+				case 'R': process += stream [b]; break;
+				case 'S': process += stream [b]; break;
+				case 'T': process += stream [b]; break;
+				case 'U': process += stream [b]; break;
+				case 'V': process += stream [b]; break;
+				case 'W': process += stream [b]; break;
+				case 'X': process += stream [b]; break;
+				case 'Y': process += stream [b]; break;
+				case 'Z': process += stream [b]; break;
+				case '-': process += stream [b]; break;
+				case '_': process += stream [b]; break;
+				case '!': process += stream [b]; break;
+				case '$': process += stream [b]; break;
+				case '?': process += stream [b]; break;
+				case '&': process += stream [b]; break;
+				case '*': process += stream [b]; break;
+				case '^': process += stream [b]; break;
+				case '~': process += stream [b]; break;
+				case '\'': process += stream [b]; break;
+				case '"': process += stream [b]; break;
+				case '|': process += stream [b]; break;
+				case ',': process += stream [b]; break;
+				case '@': process += stream [b]; break;
+				case '%': process += stream [b]; break;
+				case '\\': process += stream [b]; break;
+				case '/': process += stream [b]; break;
+				case '0': process += stream [b]; break;
+				case '1': process += stream [b]; break;
+				case '2': process += stream [b]; break;
+				case '3': process += stream [b]; break;
+				case '4': process += stream [b]; break;
+				case '5': process += stream [b]; break;
+				case '6': process += stream [b]; break;
+				case '7': process += stream [b]; break;
+				case '8': process += stream [b]; break;
+				case '9': process += stream [b]; break;
+				}
+			}
+			// creates an empty C# script in the directory.
+			File.Create(path).Close();
+
+			// and then it refreshes unity to find the created file.
+			AssetDatabase.Refresh ();
+
+			// sets all processed scripts in the generated file.
+			File.AppendAllText (path, process);
+
+			// and then it refreshes Unity to find the C# script.
+			AssetDatabase.Refresh ();
+
+			// // refreshing data.
+			check = true;
+			group = true;
+			refresh ();
+		}
+		catch{
+			// to show the dialog page in unity.
+			if(path == "") EditorUtility.DisplayDialog ("Warning", "\nابتدا یک فایل متنی اتخاب کنید\n\nChoose a file for process\n", "ok");
+			else EditorUtility.DisplayDialog ("Note", "\nیک چیزی اشتباه است\n\nSomething is wrong\n", "ok");
+		}
+	}
+
+	// refreshing method.
+	private void refresh()
+	{
+		// refreshing variables.
+		process = "";
+		stream = "";
+		file = null;
+		path = "";
+		space = 1;
+	}
+	// refresh this package.
+	internal void refreshing()
+	{
+		// // refreshing data.
+		check = false;
+		group = false;
+		refresh ();
+	}
+}
